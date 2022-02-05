c945e10f90e8f25693a42acc388b70ed57518dda
Saksham Srivastava <84475573+I-Am-Saksham-Srivastava@users.noreply.github.com>
2022-02-05 11:50:50 +0530

    Image Serialization
    Seializes Images in a Directory using Python code

---
+++ ImageSerializer.py
@@ -0,0 +1,25 @@
+
+
+import os
+
+dic = []
+
+f = 1
+
+image_dir = r"F:\pic1"  # Add Your Folder Path
+
+all_files = os.listdir(image_dir)
+
+dic = all_files
+
+for i in all_files:
+
+    path = os.path.join(image_dir,i)
+
+    a = f"{f}.jpg"  # Add Extension
+
+    p = os.path.join(image_dir, a)
+
+    f = f+1
+
+    os.rename(path, p)
